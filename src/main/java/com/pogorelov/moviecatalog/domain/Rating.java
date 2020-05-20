f7c35eb642a07e797358bbfa1c49f6ec03a41f52
opohorelov <oleksandr.pohorelov@jevera.software>
2020-05-20 14:20:43 +0300

    Step 2

---
+++ src/main/java/com/pogorelov/moviecatalog/domain/Rating.java
@@ -0,0 +1,15 @@
+package com.pogorelov.moviecatalog.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Rating {
+
+    private String movieId;
+
+    private int rating;
+
+
+}
--- src/main/java/com/pogorelov/moviecatalog/web/MovieCatalogResource.java
+++ src/main/java/com/pogorelov/moviecatalog/web/MovieCatalogResource.java
@@ -1,13 +1,14 @@
 package com.pogorelov.moviecatalog.web;
 
 import com.pogorelov.moviecatalog.domain.CatalogItem;
+import com.pogorelov.moviecatalog.domain.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,6 +16,13 @@ public class MovieCatalogResource {
 
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalogForUser(@PathVariable String userId) {
-        return List.of(new CatalogItem("Transformers", "test", 4));
+        List<Rating> ratings = List.of(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+
+        return ratings.stream()
+                .map(rating -> new CatalogItem("Transformer", "Test", 4))
+                .collect(Collectors.toList());
     }
 }
