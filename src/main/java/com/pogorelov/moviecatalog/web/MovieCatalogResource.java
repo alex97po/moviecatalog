7dca3bed806159fa54d1b1fc18b0ba9a4f25f884
opohorelov <oleksandr.pohorelov@jevera.software>
2020-05-20 14:43:50 +0300

    Step 4

--- src/main/java/com/pogorelov/moviecatalog/web/MovieCatalogResource.java
+++ src/main/java/com/pogorelov/moviecatalog/web/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.pogorelov.moviecatalog.web;
 import com.pogorelov.moviecatalog.domain.CatalogItem;
 import com.pogorelov.moviecatalog.domain.Movie;
 import com.pogorelov.moviecatalog.domain.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,11 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalogForUser(@PathVariable String userId) {
-        RestTemplate restTemplate = new RestTemplate();
 
         List<Rating> ratings = List.of(
                 new Rating("1234", 4),
