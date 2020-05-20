fed04cf88f6b8b81bdc69416004f1debc9e43d43
opohorelov <oleksandr.pohorelov@jevera.software>
2020-05-20 15:16:02 +0300

    Step 6

--- src/main/java/com/pogorelov/moviecatalog/domain/Rating.java
+++ src/main/java/com/pogorelov/moviecatalog/domain/Rating.java
@@ -2,9 +2,11 @@ package com.pogorelov.moviecatalog.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Rating {
 
     private String movieId;
--- src/main/java/com/pogorelov/moviecatalog/web/MovieCatalogResource.java
+++ src/main/java/com/pogorelov/moviecatalog/web/MovieCatalogResource.java
@@ -3,14 +3,17 @@ package com.pogorelov.moviecatalog.web;
 import com.pogorelov.moviecatalog.domain.CatalogItem;
 import com.pogorelov.moviecatalog.domain.Movie;
 import com.pogorelov.moviecatalog.domain.Rating;
+import com.pogorelov.moviecatalog.web.vm.RatingVM;
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,26 +24,15 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private WebClient.Builder webClientBuilder;
-
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalogForUser(@PathVariable String userId) {
 
-        List<Rating> ratings = List.of(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        RatingVM userRatings = restTemplate.getForObject("http://localhost:8082/ratings/users/" + userId, RatingVM.class);
+        List<Rating> ratings = userRatings.getRatings();
 
         return ratings.stream()
                 .map(rating -> {
-//                    Movie movie =restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
-                    Movie movie = webClientBuilder.build()
-                            .get()
-                            .uri("http://localhost:8081/movies/" + rating.getMovieId())
-                            .retrieve()
-                            .bodyToMono(Movie.class)
-                            .block();
+                    Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getName(), "desc", rating.getRating());
                 })
                 .collect(Collectors.toList());
